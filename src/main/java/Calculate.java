9b8fdb8cf14193913b0efe999b8d3a31c1e53a36
ZinovievIv <103179359+ZinovievIv@users.noreply.github.com>
2023-03-26 23:42:03 +0300

    Дополнил ввод товара и суммы

--- src/main/java/Calculate.java
+++
@@ -1,6 +0,0 @@
-public class Calculate {
-    Calculate calculater = new Calculate();
-    public static double calculater(double a, double b){
-        return a/b;
-    }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner inPeople = new Scanner(System.in);
-        Scanner chek = new Scanner(System.in);
         System.out.println("На сколько человек необходимо разделить счет?");
         int quantityPeople = inPeople.nextInt();
         System.out.println("Вы ввели - " + quantityPeople);
@@ -12,15 +11,22 @@ public class Main {
             System.out.println("Число должно быть больше 1, введите еще раз:");
             quantityPeople = inPeople.nextInt();
             if (quantityPeople > 1)
-                Calculate.calculater(summa, quantityPeople);
+                break;
         }
+        System.out.println("Введите наименование товара:");
+        Scanner product = new Scanner(System.in);
+        String inproduct = product.next();
+        System.out.println("Введите сумму товара");
+        Scanner price = new Scanner(System.in);
+        double inprice = price.nextDouble();
 
     }
 
     class Calculate {
         Calculate calculater = new Calculate();
-        public static double calculater(double a, double b){
-            return a/b;
+
+        public double calculater(double a, double b) {
+            return a / b;
         }
     }
 }
