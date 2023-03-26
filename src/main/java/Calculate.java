25867616ef248d427eed08bebf57a10e6dacc8c7
ZinovievIv <103179359+ZinovievIv@users.noreply.github.com>
2023-03-26 23:01:44 +0300

    Версия с реализованным калькулятором

---
+++ src/main/java/Calculate.java
@@ -0,0 +1,6 @@
+public class Calculate {
+    Calculate calculater = new Calculate();
+    public static double calculater(double a, double b){
+        return a/b;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner inPeople = new Scanner(System.in);
+        Scanner chek = new Scanner(System.in);
         System.out.println("На сколько человек необходимо разделить счет?");
         int quantityPeople = inPeople.nextInt();
         System.out.println("Вы ввели - " + quantityPeople);
-
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-
+        while (quantityPeople <= 1) {
+            System.out.println("Число должно быть больше 1, введите еще раз:");
+            quantityPeople = inPeople.nextInt();
+            if (quantityPeople > 1)
+                break;
+        }
+        System.out.println("Введите сумму счета");
+        double summa = chek.nextInt();
+        double resultat = Calculate.calculater(summa, quantityPeople);
+        System.out.println("Итого на человека:" + resultat);
     }
 }
