fdfe9677909d775bf46bd8659925279652732fc7
ZinovievIv <103179359+ZinovievIv@users.noreply.github.com>
2023-03-26 16:08:31 +0300

    Создание ввода. Начало.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 // dev branch for Y.Practicum
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         Scanner inPeople = new Scanner(System.in);
         System.out.println("На сколько человек необходимо разделить счет?");
-
-
+        int quantityPeople = inPeople.nextInt();
+        System.out.println("Вы ввели - " + quantityPeople);
 
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
 
