9528e75823d7788784427b74dbc79e93e3eae358
ZinovievIv <103179359+ZinovievIv@users.noreply.github.com>
2023-03-26 15:08:10 +0300

    Создание ввода. Начало.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
+        Scanner inPeople = new Scanner(System.in);
+        System.out.println("На сколько человек необходимо разделить счет?");
+
+
+
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
     }
 }
