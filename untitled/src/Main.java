c871dac0e99537aee192bd3015fb8c9cb38ebaea
Зиновьев Иван <zinovev.ivan@mail.ru>
2023-08-09 12:18:54 +0300

    Кофемашина

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,26 +1,21 @@
-import java.util.Date;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя");
-        String name = scanner.next();
-        System.out.println("Введите возраст");
-        int age = scanner.nextInt();
-        System.out.println("Введите Пол");
-        String pol = scanner.next();
 
-        if (age >= 18) {
-            Adult person1 = new Adult(name, age, pol);
-            System.out.println("Создан пол " + pol + " Имя = " + name + " Возраст = " + age);
-            person1.action();
-        } else {
-            Сhild person2 = new Сhild(name, age, pol);
-            System.out.println("Создан пол " + pol + " Имя = " + name + " Возраст = " + age);
-            person2.action();
+        class Pair<F, S> {
+            private F first;
+            private S second;
+
+            Pair(F first, S second) {
+                this.first = first;
+                this.second = second;
+            }
+
+            static<F, S> Pair<F, S> of(F first, S second) {
+                return new Pair<F, S>(first,second);
+            }
+
+            @Override
+            public String toString() {
+                return "Pair{" + first + " " + second + "}";
+            }
         }
-    }
-}
-
 
